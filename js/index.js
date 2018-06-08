aeeaba1fb2f252f55a3c8dc3bc705a5ec299fc52
Simon <Simon_Freelance@hotmail.com>
2018-06-08 15:38:10 -0400

    Hamburger now changes states when clicked on

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-// JS goes here
+
+const components = {
+    hamburger: document.querySelector('.nav-main__hamburger'),
+};
+
+class Hamburger {
+    constructor(element) {
+        this.element = element;
+        this.btnStates = {
+            hamburgerState: 'img/nav-hamburger.png',
+            xState: 'img/nav-hamburger-close.png'
+        };
+
+        this.element.addEventListener('click', function() {
+            if(this.element.getAttribute('src') === this.btnStates.hamburgerState) {
+                this.element.setAttribute('src', this.btnStates.xState);
+            }
+            else {
+                this.element.setAttribute('src', this.btnStates.hamburgerState);
+            }
+        }.bind(this));
+    }
+}
+
+components.hamburger = new Hamburger(components.hamburger);
